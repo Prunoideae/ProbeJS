9d4f361c4cdf29edcd3dd29bad8a4a5c01730939
Prunoideae <a455167189@163.com>
2022-06-18 16:20:59 +0800

    Multiblocked support

---
+++ common/src/main/resources/docs/multiblocked.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @mod multiblocked
+ */
+class RecipeHolder {
+    readonly multiblocked: Document.MultiblockedRecipes;
+}
+
+/**
+ * @mod multiblocked
+ */
+class MultiblockedRecipes {
+    multiblock(recipeMap: java.lang.String): com.lowdragmc.multiblocked.api.kubejs.MultiblockRecipeJS;
+}
