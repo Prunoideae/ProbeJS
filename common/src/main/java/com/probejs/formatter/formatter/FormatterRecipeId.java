8ab574877246be5a77349e6dc24190f448e4bf52
Prunoideae <a455167189@163.com>
2022-07-01 21:16:41 +0800

    Completed recipe id dumps

--- common/src/main/java/com/probejs/formatter/formatter/FormatterRecipeId.java
+++ common/src/main/java/com/probejs/formatter/formatter/FormatterRecipeId.java
@@ -1,6 +1,8 @@
 package com.probejs.formatter.formatter;
 
 import com.google.gson.Gson;
+import dev.latvian.mods.kubejs.KubeJS;
+import dev.latvian.mods.kubejs.recipe.RecipeEventJS;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.item.crafting.RecipeManager;
@@ -9,19 +11,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class FormatterRecipeId implements IFormatter {
-    public static MinecraftServer server = null;
-
-    public static void onServerLoaded(MinecraftServer minecraftServer) {
-        server = minecraftServer;
-    }
+    public static List<ResourceLocation> originalIds = null;
 
     @Override
     public List<String> format(Integer indent, Integer stepIndent) {
-        if (server == null)
+        if (originalIds == null)
             return List.of();
-        RecipeManager manager = server.getRecipeManager();
         Gson g = new Gson();
-        return List.of("%stype RecipeId = %s;".formatted(" ".repeat(indent), manager.getRecipeIds()
+        return List.of("%stype RecipeId = %s;".formatted(" ".repeat(indent), originalIds.stream()
                 .filter(rl -> !rl.getPath().startsWith("kjs_")) //Filter out kjs-like recipes
                 .map(ResourceLocation::toString)
                 .map(g::toJson)
