d14b100dc5d878c7b7cbbb5d181b685f3e8b9952
Li Junyu <jlijh@connect.ust.hk>
2024-01-03 01:26:06 +0800

    Fixed #45

--- common/src/main/java/com/probejs/rich/fluid/FluidAttribute.java
+++ common/src/main/java/com/probejs/rich/fluid/FluidAttribute.java
@@ -3,6 +3,7 @@ package com.probejs.rich.fluid;
 import com.probejs.util.json.JObject;
 import com.probejs.util.json.JPrimitive;
 import dev.architectury.fluid.FluidStack;
+import dev.latvian.mods.kubejs.fluid.FluidStackJS;
 import dev.latvian.mods.kubejs.registry.RegistryInfo;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
@@ -21,9 +22,18 @@ public class FluidAttribute {
     }
 
     public JObject serialize() {
+        String fluidName = "Unknown Fluid";
+        try {
+            fluidName = fluidStack.getName().getString();
+        } catch (Exception e) {
+            try {
+                fluidName = FluidStackJS.of(fluidStack).getId();
+            } catch (Exception ignored) {
+            }
+        }
         return JObject.create()
                 .add("id", JPrimitive.create(Objects.requireNonNull(RegistryInfo.FLUID.getVanillaRegistry().getKey(fluid)).toString()))
-                .add("localized", JPrimitive.create(fluidStack.getName().getString()))
+                .add("localized", JPrimitive.create(fluidName))
                 .add("hasBucket", JPrimitive.create(fluid.getBucket() != Items.AIR))
                 .add("hasBlock", JPrimitive.create(fluid.defaultFluidState().createLegacyBlock().getBlock() != Blocks.AIR))
                 .add("bucketItem", JPrimitive.create(Objects.requireNonNull(RegistryInfo.ITEM.getVanillaRegistry().getKey(fluid.getBucket())).toString()));
