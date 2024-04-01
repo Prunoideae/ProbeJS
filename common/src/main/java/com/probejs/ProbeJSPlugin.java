20999319d40ce9ef69cf9025273dd84b72ac6420
Li Junyu <jlijh@connect.ust.hk>
2024-04-01 21:56:30 +0800

    Updated to v6.0.1

--- common/src/main/java/com/probejs/ProbeJSPlugin.java
+++ common/src/main/java/com/probejs/ProbeJSPlugin.java
@@ -2,10 +2,18 @@ package com.probejs;
 
 import com.probejs.features.plugin.ProbeJSEvents;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.ScriptType;
+import dev.latvian.mods.kubejs.util.ClassFilter;
 
 public class ProbeJSPlugin extends KubeJSPlugin {
     @Override
     public void registerEvents() {
         ProbeJSEvents.GROUP.register();
+    }
+
+    @Override
+    public void registerClasses(ScriptType type, ClassFilter filter) {
+        // lol
+        filter.deny("com.probejs");
     }
 }
